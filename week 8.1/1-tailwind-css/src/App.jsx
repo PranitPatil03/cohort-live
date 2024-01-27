2eaf2c483f7e0d93e7de78b0d7da7af52f52dd4e
Pranit Patil <91155068+PranitPatil03@users.noreply.github.com>
2024-01-27 16:55:46 +0530

    Week 8 TailWind

---
+++ week 8.1/1-tailwind-css/src/App.jsx
@@ -0,0 +1,58 @@
+function App() {
+  return (
+    <>
+      {/* <div className="flex flex-nowrap">
+        <div className="basis-1/4 p-4 border-2">01</div>
+        <div className="basis-1/4 p-4 border-2">02</div>
+        <div className="basis-1/2 p-4 border-2 hover:basis-1/2">03</div>
+        </div> */}
+
+      {/* <div className="flex flex-nowrap">
+        <div className="p-4 border-2">01</div>
+        <div className="p-4 border-2">02</div>
+        <div className="p-4 border-2">03</div>
+      </div>*/}
+
+      {/* <div className="grid grid-cols-4 gap-4">
+        <div className="p-4 border-2">01</div>
+        <div className="p-4 border-2">02</div>
+        <div className="p-4 border-2">03</div>
+        <div className="p-4 border-2">01</div>
+        <div className="p-4 border-2">02</div>
+        <div className="p-4 border-2">03</div>
+        <div className="p-4 border-2">01</div>
+        <div className="p-4 border-2">02</div>
+        <div className="p-4 border-2">03</div>
+      </div>*/}
+
+      {/* <div className="text-center sm:text-left">
+        <div className="p-4 border-2">Pranit</div>
+      </div>
+      <div className="sm:text-center">
+        <div className="p-4 border-2">Pranit</div>
+      </div>
+      */}
+
+      {/* <div className="flex flex-col md:flex-row">
+        <div className="basis-1/4 p-4 border-2 text-center">01</div>
+        <div className="basis-1/4 p-4 border-2 text-center">02</div>
+        <div className="basis-1/4 p-4 border-2 text-center">03</div>
+        <div className="basis-1/4 p-4 border-2 text-center">04</div>
+      </div>*/}
+
+      <div className="mx-5 my-5 grid grid-cols-1 lg:grid-cols-4 gap-4">
+        <div className="p-4 border-2 text-center">01</div>
+        <div className="p-4 border-2 text-center">02</div>
+        <div className="p-4 border-2 text-center">03</div>
+        <div className="p-4 border-2 text-center">01</div>
+        <div className="p-4 border-2 text-center">02</div>
+        <div className="p-4 border-2 text-center">03</div>
+        <div className="p-4 border-2 text-center">01</div>
+        <div className="p-4 border-2 text-center">02</div>
+        <div className="p-4 border-2 text-center">03</div>
+      </div>
+    </>
+  );
+}
+
+export default App;
